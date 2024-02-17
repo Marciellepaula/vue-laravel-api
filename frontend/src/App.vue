87f3845e2a168019e684f9b235806ebe5726b9e9
marcielle <marcielle0644@gmail.comm>
2024-02-17 15:38:35 -0300

    aprendendo vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,31 @@
-<script setup>
-import Student from './components/Student.vue'
-</script>
 <template>
-  <Student />
+  <div>
+    <div>
+      <input v-model="user.first_name" type="text">
+    </div>
+
+    <!-- ... other code ... -->
+
+  </div>
 </template>
+
+
+
+
+
+<script setup>
+
+import { computed, ref } from 'vue';
+
+const user = ref(value: {
+  first_name: 'jonh',
+  last_name: 'snow'
+})
+
+const fullname = computed(getter: () => {
+  return `${user.value.first_name}`;
+})
+
+
+const total = computed(get: () => products.reduce)
+</script>
